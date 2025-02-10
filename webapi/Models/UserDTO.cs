8447e01f8adf9fd5549c50921c2d3e5d41ce7d06
leslie <devotaleslie@gmail.com>
2025-02-10 15:27:37 +0100

    Creation of UserDTO Webapi entity

---
+++ webapi/Models/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using BLL.Entities;
+
+namespace webapi.Models
+{
+	public class UserDTO
+	{
+		public Guid User_Id { get; set; }
+		public string First_Name { get; set; }
+		public string Last_Name { get; set; }
+		public string Email { get; set; }
+		public DateTime CreatedAt { get; set; }
+
+		public bool IsDisable { get; set; } 
+
+
+	}
+}
