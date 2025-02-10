fde479d0b74435eb5fbddd25cad52003363eda17
leslie <devotaleslie@gmail.com>
2025-02-10 11:25:28 +0100

    feat: add Student model and update DemoController to return a list of students

--- webapi/Controllers/DemoController.cs
+++ webapi/Controllers/DemoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
 
 namespace webapi.Controllers
 {
@@ -7,15 +8,39 @@ namespace webapi.Controllers
 	[ApiController]
 	public class DemoController : ControllerBase
 	{
-		private static List<string> _list = new List<string>
+		private static List<Student> _list = new List<Student>
 		{
-			"Charifa", "Khaoula", "Mélusine", "Dorothée", "Jenny", "Marwa", "Anaïs", "Emilie", "Amalia", "Leslie", "Debby"
+			new Student(){
+				Student_Id = 1, First_Name = "Charifa", Last_Name  = "Zhinef" },
+			new Student(){ 
+				Student_Id = 2, First_Name = "Khaoula", Last_Name  = "Ayra" },
+			new Student(){
+				Student_Id = 3, First_Name = "Mélusine", Last_Name  = "Khaoula" },
+			new Student(){
+				Student_Id = 4, First_Name = "Dorothée", Last_Name  = "Valentyn" },
+			new Student(){
+				Student_Id = 5, First_Name = "Jenny", Last_Name  = "Fernandez Garcia" },
+			new Student(){
+				Student_Id = 6, First_Name = "Marwa", Last_Name  = "Khaoula" },
+			new Student(){
+				Student_Id = 7, First_Name = "Anaïs", Last_Name  = "Aerts" },
+			new Student(){
+				Student_Id = 8, First_Name = "Emilie", Last_Name  = "Blanckaerts" },
+			new Student(){
+				Student_Id = 9, First_Name = "Amalia", Last_Name  = "Langlet Tessaro" },
+			new Student(){
+				Student_Id = 10, First_Name = "Leslie", Last_Name  = "Habimana" },
+			new Student(){
+				Student_Id = 11, First_Name = "Jessica", Last_Name  = "Conrad" },
+			new Student(){
+				Student_Id = 12, First_Name = "Debby", Last_Name  = "Clerckx" },
+
 		};
 		
 		[HttpGet("")]
-		public ActionResult<List<string>> Get()
+		public ActionResult<List<Student>> Get()
 		{
-			List<string> model = _list;
+			List<Student> model = _list;
 			return Ok(model);
 		}
 	}
